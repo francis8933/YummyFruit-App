ea958753bc579f04aadcecc75fcd3efafec45bf3
francis8933 <francisespinoza33@gmail.com>
2023-06-14 07:40:27 -0400

    working on proxy

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  //mode: "development",
+  mode: process.env.NODE_ENV,
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+    clean: true, // Clean the output directory before emit.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", "@babel/preset-react"]],
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  //devtool: 'inline-source-map',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      //ask is title is require
+      //title: 'Production' or 'development,
+    }),
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+    //DefinePlugin to value development/production. Enables useful names for modules and chunks.
+    // new webpack.DefinePlugin({
+    //   "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    // }),
+  ],
+  devServer: {
+    // historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, "./build"),
+    // open: true,
+    // compress: true,
+    // hot: true,
+    // port: 8080,
+  },
+};
